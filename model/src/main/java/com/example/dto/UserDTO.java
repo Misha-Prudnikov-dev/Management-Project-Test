b439c0482c6ae6a46b3cd8e26b1b6f4ac05feae8
Misha-Prudnikov-dev <54108720+Misha-Prudnikov-dev@users.noreply.github.com>
2024-01-23 20:25:21 +0300

    Add files via upload
    added model

---
+++ model/src/main/java/com/example/dto/UserDTO.java
@@ -0,0 +1,82 @@
+package com.example.dto;
+
+import jakarta.validation.constraints.*;
+
+
+public class UserDTO {
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2,max = 40,message = "Name should be between 2 and 40")
+    private String name;
+
+    @NotEmpty(message = "surname should not be empty")
+    @Size(min = 2,max = 40,message = "surname should be between 2 and 40")
+    private String surname;
+
+    @NotEmpty(message = "patronymic should not be empty")
+    @Size(min = 2,max = 40,message = "patronymic should be between 2 and 40")
+    private String patronymic;
+
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Min(value = 1,message = "not 0")
+    private Long roleId;
+
+     public UserDTO() {
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", email='" + email + '\'' +
+                ", roleId=" + roleId +
+                '}';
+    }
+}
---
+++ model/src/main/java/com/example/entity/User.java
@@ -0,0 +1,169 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+
+    /**
+     *
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_users")
+    private Long id;
+
+    @Column(name = "name_users")
+    private String name;
+
+    @Column(name = "surname_users")
+    private String surname;
+
+    @Column(name = "patronymic_users")
+    private String patronymic;
+
+    @Column(name = "email_users")
+    private String email;
+
+
+    @ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+    @JoinTable(name = "roles_has_users", joinColumns = @JoinColumn(name = "users_id_users"), inverseJoinColumns = @JoinColumn(name = "roles_id_roles"))
+    private Role role;
+
+    public User() {
+
+    }
+
+
+    public User(Long id, String name, String surname, String patronymic, String email,Role role) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.email = email;
+        this.role=role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((patronymic == null) ? 0 : patronymic.hashCode());
+        result = prime * result + ((role == null) ? 0 : role.hashCode());
+        result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (patronymic == null) {
+            if (other.patronymic != null)
+                return false;
+        } else if (!patronymic.equals(other.patronymic))
+            return false;
+        if (role == null) {
+            if (other.role != null)
+                return false;
+        } else if (!role.equals(other.role))
+            return false;
+        if (surname == null) {
+            if (other.surname != null)
+                return false;
+        } else if (!surname.equals(other.surname))
+            return false;
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
+
